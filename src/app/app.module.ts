14ed3523f33ce876c14000ce707787da83fe4535
Vladislav <vladikbirkos@yandex.ru>
2019-03-31 16:20:25 +0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router'; //connect routes
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';//connect bootstrap
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; //connect bootstrap
 import { HeaderComponent } from './header/header.component';
 import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component'; 
+import { FooterComponent } from './footer/footer.component';
+import { WorkComponent } from './work/work.component';
+import { ResumeComponent } from './resume/resume.component'; 
+
+const appRoutes: Routes = [
+  {path:'', component: BodyComponent},  
+  {path:'work', component: WorkComponent},
+  {path:'resume', component: ResumeComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    WorkComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot() //connect bootstrap
+    NgbModule.forRoot(), //connect bootstrap
+    RouterModule.forRoot(appRoutes) //connect routes
   ],
   providers: [],
   bootstrap: [AppComponent]
